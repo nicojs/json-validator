a43fdd74c56115966f1b97e8eb026844ce976a62
Nico Jansen <nico.jansen@infosupport.com>
2019-09-03 08:07:51 +0200

    Add travis build file

---
+++ tasks/clean.sh
@@ -0,0 +1,2 @@
+ls -A | grep --invert-match dist | grep --invert-match -F -w .git | xargs rm -rf # Remove all but "dist" and ".git"
+mv dist/* .
--- tasks/publish.sh
+++ tasks/publish.sh
@@ -1,6 +1,7 @@
-# Remove all but "dist"
-ls | grep --invert-match dist | xargs rm -rf
-
+git remote add gh-publish https://${GIT_TOKEN}@github.com/stryker-mutator/stryker-mutator.github.io.git
+git checkout --track -b gh-pages gh-publish/master
+ls -A | grep --invert-match dist | grep --invert-match -F -w .git | xargs rm -rf # Remove all but "dist" and ".git"
 mv dist/* .
-git checkout gh-pages
-git push origin gh-pages
+git add .
+git commit -m "Publish"
+git push
